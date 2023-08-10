423648478000bd3b08d0113f55ca3fcb66e78136
Anton Adamkovich <anton.adamkovich@clear.bank>
2023-08-10 05:14:16 -0700

    refactor styles for region switch

--- src/components/region-switch/region-switch.tsx
+++ src/components/region-switch/region-switch.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import Toggle from 'react-toggle';
+import './region-switch.styles.css'
 
 export default function RegionSwitch() {
   const [activeOption, setActiveOption] = useState<boolean>(document.location.href.includes('/eu'));
