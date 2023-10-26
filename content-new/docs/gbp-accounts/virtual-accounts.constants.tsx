91640680b98fad8434e4d09763237743bd9e3dd5
KatKent <104571437+KatKent@users.noreply.github.com>
2023-10-26 09:25:21 +0100

    Removed statement that X-Request-Id is used for idempotency.

--- content-new/docs/gbp-accounts/virtual-accounts.constants.tsx
+++ content-new/docs/gbp-accounts/virtual-accounts.constants.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const postVirtualAccountsDescription = (
   <>
     <p>
-      This endpoint uses accountIdentifier as a duplicate check and to ensure the request is idempotent.
+      This endpoint uses accountIdentifier as a duplicate check.
     </p>
   </>
 );
