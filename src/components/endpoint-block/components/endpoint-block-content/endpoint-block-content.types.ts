521028d6dc41e7fd10c0cbf0accfa69d65e9e399
Chris Wood <chris@arborcomputing.com>
2024-05-17 10:21:43 +0100

    Dev Portal support for displaying oneOf in payload definitions
    Add support for displaying OneOf construct in endpoint definitions on the Developer Portal. No change to documentation or API functionality.
    
    * feat: Add support for oneOf in generating description and labels
    
    * chore: Updated snapshot and added Oxford comma
    
    * feat: Added support for oneOf in schema object display
    
    ---------
    
    Co-authored-by: Chris Wood <chris.wood@clear.bank>

--- src/components/endpoint-block/components/endpoint-block-content/endpoint-block-content.types.ts
+++ src/components/endpoint-block/components/endpoint-block-content/endpoint-block-content.types.ts
@@ -1,4 +1,11 @@
 export interface EndpointBlockContentProps {
   allProperties: any
   requiredPropertyKeys: any
+  propertyGroups?: object
+}
+
+export interface OneOfProps {
+  type: string,
+  required: Array<string>,
+  properties: Object,
 }
