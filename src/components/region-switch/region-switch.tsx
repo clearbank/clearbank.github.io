edd28b818572133f4c9658dffd3f699d962d6e44
Paul-Dan Pruteanu <paul-dan.pruteanu@clear.bank>
2024-08-15 16:25:40 +0100

    gracefully fails over undefined document object

--- src/components/region-switch/region-switch.tsx
+++ src/components/region-switch/region-switch.tsx
@@ -3,12 +3,12 @@ import Toggle from 'react-toggle';
 import './region-switch.styles.css'
 
 export default function RegionSwitch() {
-  const [activeOption, setActiveOption] = useState<boolean>(document.location.href.includes('/eu'));
+  const [activeOption, setActiveOption] = useState<boolean>(document?.location.href.includes('/eu'));
 
   const handleSwitchClick = () => {
     setActiveOption(!activeOption);
 
-    setTimeout(() => document.location.href = !activeOption ? '/eu' : '/uk', 300);
+    setTimeout(() => document?.location.href = !activeOption ? '/eu' : '/uk', 300);
   };
 
   let region;
