e3d5ddbc73687aa39f4692eb6867ba25f3e5c189
cleareth <elliott.hughes@clear.bank>
2025-02-14 13:27:45 +0000

    implement open-in-new icon

--- src/components/external-link/external-link.tsx
+++ src/components/external-link/external-link.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { ExternalLinkProps } from './external-link.types';
 import { ExternalLinkIcon } from './external-link-style';
+import { ReactComponent as ExternalLinkSvg } from '../../assets/svgs/open.in.new.svg';
 
 const ExternalLink: React.FC<ExternalLinkProps> = ({ href, children }) => {
   return (
     <a href={href} target="_blank" rel="noopener noreferrer">
       {children}
-      <ExternalLinkIcon />
+      <ExternalLinkIcon>
+        <ExternalLinkSvg />
+      </ExternalLinkIcon>
     </a>
   );
 };
