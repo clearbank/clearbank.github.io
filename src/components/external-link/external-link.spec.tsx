e85dbd00e7d09b259c27753ab0416ceac309ae36
cleareth <elliott.hughes@clear.bank>
2025-02-18 10:31:24 +0000

    fix link-text for automated test

--- src/components/external-link/external-link.spec.tsx
+++ src/components/external-link/external-link.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import ExternalLink from './external-link';
 
 test('renders external link with icon', () => {
-  const { getByText } = render(<ExternalLink href="https://gov.uk">Example</ExternalLink>);
+  const { getByText } = render(<ExternalLink href="https://gov.uk">Example text</ExternalLink>);
   const linkElement = getByText(/Example text/i);
   expect(linkElement).toBeInTheDocument();
   expect(linkElement).toHaveAttribute('href', 'https://gov.uk');
