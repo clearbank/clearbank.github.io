e07b2cadb67fd9f2a539ff88e0be7a381214c414
sharon-clears <sharon.adewusi@clear.bank>
2026-09-22 14:49:30 +0100

    technical refs batch 2 - eu covered

--- content/uk/docs/gbp-payments/managing-direct-debit-instructions.constants.tsx
+++ content/uk/docs/gbp-payments/managing-direct-debit-instructions.constants.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const postCreateDdiForAccountDescription = (
   <>
     <p>
-      This endpoint uses a combination of the SUN and reference as a duplicate check and to ensure the request is idempotent.
+      This endpoint uses a combination of the Service User Number (SUN) and reference as a duplicate check and to ensure the request is idempotent.
     </p>
   </>
 );
